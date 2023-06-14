e3c8d0b82404c673cb225a66373ce728c58f6941
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-06-14 14:36:12 +0200

    change indents

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  Link,
-  RouterProvider,
-  Outlet,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    Link,
+    RouterProvider,
+    Outlet,
 } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
@@ -12,29 +12,29 @@ import SingleTypePage from "./pages/SingleTypePage";
 import Breadcrumbs from "./components/Breadcrumbs";
 
 const Root = () => {
-  return (
-    <>
-      <div>
-        <Breadcrumbs />
-      </div>
-      <div>
-        <Outlet />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Breadcrumbs/>
+            </div>
+            <div>
+                <Outlet/>
+            </div>
+        </>
+    );
 };
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route index element={<Home />} />
-      <Route path={":pokemonType/:pokemon?"} element={<SingleTypePage />} />
-    </Route>
-  )
+    createRoutesFromElements(
+        <Route path="/" element={<Root/>}>
+            <Route index element={<Home/>}/>
+            <Route path={":pokemonType/:pokemon?"} element={<SingleTypePage/>}/>
+        </Route>
+    )
 );
 
 function App() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router}/>;
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,37 @@
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 import getPokemonTypes from "../services/getTypes";
 
 type SingleType = {
-  name: string;
-  url: string;
+    name: string;
+    url: string;
 };
 
 function Home() {
-  const [types, setTypes] = useState<SingleType[]>([]);
+    const [types, setTypes] = useState<SingleType[]>([]);
 
-  useEffect(() => {
-    getPokemonTypes().then((result) =>
-      setTypes(result?.data.results as SingleType[])
+    useEffect(() => {
+        getPokemonTypes().then((result) =>
+            setTypes(result?.data.results as SingleType[])
+        );
+    }, []);
+
+    return (
+        <div>
+            <h1>Choose pokemon type:</h1>
+            <ul>
+                {types.map((type) => (
+                    <li>
+                        <Link key={type.name} to={`/${type.name}`}>
+                            {type.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+
+            <button onClick={() => console.log("types", types)}>test</button>
+        </div>
     );
-  }, []);
-
-  return (
-    <div>
-      <h1>Choose pokemon type:</h1>
-      <ul>
-        {types.map((type) => (
-          <li>
-            <Link key={type.name} to={`/${type.name}`}>
-              {type.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-
-      <button onClick={() => console.log("types", types)}>test</button>
-    </div>
-  );
 }
 
 export default Home;
